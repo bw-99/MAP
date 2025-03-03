7ffc1c2f6d8fc0b8dd87157731d230751bc6039e
dwen-watcha <dwen@watcha.com>
2025-03-03 18:38:40 +0900

    add: read and set reconstruction config

--- graphrag/config/models/sentence_reconstruction_config.py
+++ graphrag/config/models/sentence_reconstruction_config.py
@@ -17,32 +17,27 @@ class SentenceReconstructionConfig(LLMConfig):
     prompt: str | None = Field(
         description="The sentence reconstruction prompt to use.", default=None
     )
-    sentence_reconstruction: bool = Field(
+    enabled: bool = Field(
         default=True, description="Whether to reconstruct the sentences."
     )
     strategy: dict | None = Field(
         description="Override the default entity extraction strategy", default=None
     )
     
-    # TODO: update the resolved_strategy method
-    def resolved_strategy(self, root_dir: str, encoding_model: str | None) -> dict:
+    def resolved_strategy(self, root_dir: str) -> dict:
         """Get the resolved entity extraction strategy."""
-        from graphrag.index.operations.extract_entities import (
-            ExtractEntityStrategyType,
+        from graphrag.index.operations.reconstruct_sentence import (
+            SentenceReconstructionStrategyType,
         )
         
         return self.strategy or {
-            "type": ExtractEntityStrategyType.graph_intelligence,
+            "type": SentenceReconstructionStrategyType.plain_llm,
             "llm": self.llm.model_dump(),
             **self.parallelization.model_dump(),
-            "extraction_prompt": (Path(root_dir) / self.prompt)
+            "reconstruction_prompt": (Path(root_dir) / self.prompt)
             .read_bytes()
             .decode(encoding="utf-8")
             if self.prompt
             else None,
-            "max_gleanings": self.max_gleanings,
-            # It's prechunked in create_base_text_units
-            "encoding_name": encoding_model or self.encoding_model,
-            "prechunked": True,
-            "use_doc_id": self.use_doc_id,
+            "enabled": self.enabled
         }
