74c0482c19bd82b63599ba972616330b79d9116c
dwen-watcha <dwen@watcha.com>
2025-03-22 12:18:30 +0900

    add: Split evaluation mode into two separate modes

--- graphrag/cli/evaluate.py
+++ graphrag/cli/evaluate.py
@@ -14,6 +14,7 @@ import graphrag.api as api
 from graphrag.config.load_config import load_config
 from graphrag.config.models.graph_rag_config import GraphRagConfig
 from graphrag.config.resolve_path import resolve_paths
+from graphrag.evaluate.load_graph import read_graph
 from graphrag.index.create_pipeline_config import create_pipeline_config
 from graphrag.logger.print_progress import PrintProgressLogger
 from graphrag.storage.factory import StorageFactory
@@ -22,12 +23,13 @@ from graphrag.utils.cli import _resolve_output_files
 
 logger = PrintProgressLogger("")
 
-def evaluate_cli(
+def evaluate_index(
     config_filepath: Path | None,
     root_exp: Path,
     root_ctl: Path,
     response_type: str,
     dry_run: bool,
+    evaluate_files: list[str] = ["create_final_nodes.parquet", "create_final_entities.parquet", "create_final_communities.parquet", "create_final_community_reports.parquet"]
 ):
     """Run the pipeline with the given config."""
     root = root_exp.resolve()
@@ -35,60 +37,8 @@ def evaluate_cli(
 
     root = root_ctl.resolve()
     config_ctl = load_config(root, config_filepath)
-    _run_evaluate(
-        config_exp=config_exp,
-        config_ctl=config_ctl,
-        dry_run=dry_run,
-        response_type=response_type,
-    )
 
-def _run_evaluate(
-    config_exp,
-    config_ctl,
-    dry_run,
-    response_type,
-):
-    """Perform the actual pipeline to evaluate LLM responses.
-
-    Loads index files required for evaluation and runs the evaluation pipeline."""
-
-    # config.storage.base_dir = str(data_dir) if data_dir else config.storage.base_dir
-    resolve_paths(config_exp)
-    resolve_paths(config_ctl)
-
-    dataframe_dict_exp = _resolve_output_files(
-        config=config_exp,
-        output_list=[
-            "create_final_nodes.parquet",
-            "create_final_entities.parquet",
-            "create_final_communities.parquet",
-            "create_final_community_reports.parquet",
-        ],
-        optional_list=[],
-    )
-    final_nodes_exp: pd.DataFrame = dataframe_dict_exp["create_final_nodes"]
-    final_entities_exp: pd.DataFrame = dataframe_dict_exp["create_final_entities"]
-    final_communities_exp: pd.DataFrame = dataframe_dict_exp["create_final_communities"]
-    final_community_reports_exp: pd.DataFrame = dataframe_dict_exp[
-        "create_final_community_reports"
-    ]
-
-    dataframe_dict_ctl = _resolve_output_files(
-        config=config_ctl,
-        output_list=[
-            "create_final_nodes.parquet",
-            "create_final_entities.parquet",
-            "create_final_communities.parquet",
-            "create_final_community_reports.parquet",
-        ],
-        optional_list=[],
-    )
-    final_nodes_ctl: pd.DataFrame = dataframe_dict_ctl["create_final_nodes"]
-    final_entities_ctl: pd.DataFrame = dataframe_dict_ctl["create_final_entities"]
-    final_communities_ctl: pd.DataFrame = dataframe_dict_ctl["create_final_communities"]
-    final_community_reports_ctl: pd.DataFrame = dataframe_dict_ctl[
-        "create_final_community_reports"
-    ]
+    exp_dict, ctl_dict = read_graph(config_exp, evaluate_files), read_graph(config_ctl, evaluate_files)
 
     if dry_run:
         logger.success("Dry run complete, exiting...")
@@ -97,13 +47,27 @@ def _run_evaluate(
     response, context_data = asyncio.run(
         api.evaluate_graph(
             config=(config_exp, config_ctl),
-            nodes=(final_nodes_exp, final_nodes_ctl),
-            entities=(final_entities_exp, final_entities_ctl),
-            communities=(final_communities_exp, final_communities_ctl),
-            community_reports=(final_community_reports_exp, final_community_reports_ctl),
+            nodes=(exp_dict["create_final_nodes"], ctl_dict["create_final_nodes"]),
+            entities=(exp_dict["create_final_entities"], ctl_dict["create_final_entities"]),
+            communities=(exp_dict["create_final_communities"], ctl_dict["create_final_communities"]),
+            community_reports=(exp_dict["create_final_community_reports"], ctl_dict["create_final_community_reports"]),
             response_type=response_type,
         )
     )
 
     logger.success(f"Evaluate Response: \n{response}")
     return response, context_data
+
+
+def evaluate_query(
+    config_filepath: Path | None,
+    root_exp: Path,
+    response_type: str,
+    dry_run: bool,
+    evaluate_files: list[str] = ["create_final_nodes.parquet", "create_final_entities.parquet", "create_final_communities.parquet", "create_final_community_reports.parquet"]
+):
+    """Run the pipeline with the given config."""
+    root = root_exp.resolve()
+    config_exp = load_config(root, config_filepath)
+
+    assert False, "Not implemented yet"
