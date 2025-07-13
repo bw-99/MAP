af97792346a4a56ecd133bff52e331680ba96d50
dwen-watcha <dwen@watcha.com>
2025-07-13 13:56:40 +0900

    chore: add parsed data
    chore: add parsed data
    
    chore: fix minor

--- util/process_paper/const.py
+++ util/process_paper/const.py
@@ -7,7 +7,7 @@ TITLE_LIST = DATA_DIR / "acm_titles.txt"
 PDF_LINK_CSV = DATA_DIR / "arxiv_papers.csv"
 PARSED_DIR = DATA_DIR / "parsed"
 PDF_DIR = DATA_DIR / "pdf"
-TMP_DIR = Path("/tmp")
+TMP_DIR = Path("/tmp/map")
 
 PARSED_DIR.mkdir(exist_ok=True)
 PDF_DIR.mkdir(exist_ok=True)
