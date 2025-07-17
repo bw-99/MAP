0c40c705d205aa803a11f68528b7d3938974aa63
dwen-watcha <dwen@watcha.com>
2025-07-17 18:41:28 +0900

    add: self entity

--- graphrag/index/flows/create_final_documents.py
+++ graphrag/index/flows/create_final_documents.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import json
 from util.process_paper.const import REFERENCE_KEY
+from util.fileio import decode_paper_title
 
 def create_final_documents(
     doc_df: pd.DataFrame,
@@ -26,6 +27,10 @@ def create_final_token2doc(
     doc_refs = [
     pd.DataFrame(
             json.load(open(f"data/parsed/{fname}.json"))[REFERENCE_KEY]
+            + [{
+                "ref_id": "b0",
+                "title": decode_paper_title(fname).strip().upper()
+            }]
         ).assign(doc_id=doc_id)
         .assign(ref_id=lambda x: x["ref_id"].str.upper())
         [["ref_id", "title", "doc_id"]] for fname, doc_id in zip(doc_df["title"], doc_df["human_readable_id"])
