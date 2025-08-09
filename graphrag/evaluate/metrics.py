a850a202bb59d1b2dd155f5f3d178da9b07f4f6a
dwen-watcha <dwen@watcha.com>
2025-08-09 18:10:45 +0900

    chore: drop map metric

--- graphrag/evaluate/metrics.py
+++ graphrag/evaluate/metrics.py
@@ -6,18 +6,18 @@ def metrics_recall(topk_idx_lst: np.ndarray, gt_idx_lst: np.ndarray) -> float:
 
 
 def metrics_mrr(topk_idx_lst: np.ndarray, gt_idx_lst: np.ndarray) -> float:
-    matches = topk_idx_lst == gt_idx_lst[:, None]
-    ranks = np.argmax(matches, axis=1)
-    found = np.any(matches, axis=1)
+    matches: np.ndarray = topk_idx_lst == gt_idx_lst[:, None]
 
-    reciprocal_ranks = np.zeros_like(ranks, dtype=np.float32)
-    reciprocal_ranks[found] = 1.0 / (ranks[found] + 1)
+    nonzero_row, nonzero_col = np.where(matches)
+    mrr_per_sample = matches.astype(np.float32)
 
-    return reciprocal_ranks.mean()
+    mrr_per_sample[nonzero_row, nonzero_col] = 1 / (nonzero_col + 1)
+
+    return mrr_per_sample.sum(axis=1).mean()
 
 
 def metrics_ndcg(topk_idx_lst: np.ndarray, gt_idx_lst: np.ndarray) -> float:
-    matches = topk_idx_lst == gt_idx_lst[:, None]  # (N, K)
+    matches: np.ndarray = topk_idx_lst == gt_idx_lst[:, None]  # (N, K)
     found = np.any(matches, axis=1)
     ranks = np.argmax(matches, axis=1)  # 0-based
 
@@ -25,14 +25,3 @@ def metrics_ndcg(topk_idx_lst: np.ndarray, gt_idx_lst: np.ndarray) -> float:
     ndcg[found] = 1.0 / np.log2(ranks[found] + 2)
 
     return ndcg.mean()
-
-
-def metrics_map(topk_idx_lst: np.ndarray, gt_idx_lst: np.ndarray) -> float:
-    matches = topk_idx_lst == gt_idx_lst[:, None]  # (N, K)
-    found = np.any(matches, axis=1)
-    ranks = np.argmax(matches, axis=1)
-
-    ap = np.zeros_like(ranks, dtype=np.float32)
-    ap[found] = 1.0 / (ranks[found] + 1)
-
-    return ap.mean()
